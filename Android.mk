cee89a002b8eb350ec66d7d3e52ab019787bc717
Samarth <smrth097@gmail.com>
2026-09-25 15:46:50 +0530

    frogger: Nothing Phone (4a) [V4.1-260808-1352]
    Signed-off by: Samarth <smrth097@gmail.com>

---
+++ Android.mk
@@ -0,0 +1,32 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),frogger)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,5c5d2c68789f9c1df213507732dee9f196c77054)
+$(call add-radio-file-sha1-checked,radio/aop.img,d94cf24b8af028be9ce4612dad26e8f0f4a052b0)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,0be9f00cd295d233825806d6c1c528c69795517b)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,b3f3e816cfc219ab76fe37921ac5704d620d3947)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,1237a244759a3fa2cb2d833deb868eb16b9677f9)
+$(call add-radio-file-sha1-checked,radio/cpucp_dtb.img,1f0a286dcd44c1b7f7838eae7aa3b05514b53f41)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,88b2fac761291feaee86d41245e98516c07cc488)
+$(call add-radio-file-sha1-checked,radio/dsp.img,e9687f1666b1974f9b19ff06e993369c43912a87)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,49cfcdf87756f37fe8c8e55025bd24515bd9ab44)
+$(call add-radio-file-sha1-checked,radio/hyp.img,72fad17bc8993bd969d08440bfd960f7ed02513e)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,cf1ec00f90523495089fa19d37c4765549b86506)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,29d3e32d04b86157c1e2f97fcb07a1393b8cd1a0)
+$(call add-radio-file-sha1-checked,radio/modem.img,69c10cf15e1e2afecbc0f6901dc546553eba858a)
+$(call add-radio-file-sha1-checked,radio/multiimgoem.img,bd028305f407abeb0d9dc0d20bf64f7e63d7fc71)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,5fc3360da44b3129dd22f0fb529f7a578eff57a3)
+$(call add-radio-file-sha1-checked,radio/shrm.img,c410d82aa7060e850e204787b9b45d3440bac306)
+$(call add-radio-file-sha1-checked,radio/tz.img,7fe64141160572f557603e1e09fcc4adb6051b6d)
+$(call add-radio-file-sha1-checked,radio/uefi.img,527da5f95e7620574f91dfb590a21d495f031bdd)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,6a5003205c2f60b067406fbf470fe544219fce93)
+$(call add-radio-file-sha1-checked,radio/xbl.img,25850d4de2026a17d329b14bdcd81d31adeaf336)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,8fb536459509fe5090837aa224194e184fb7ef81)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,d8bc6a7740fb43ea64f120b59eb6ffb70ff4c8a7)
+
+endif
